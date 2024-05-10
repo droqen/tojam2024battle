d907b2ce5cfc42717f912d62dfc84042ff903fdd
droqen <hi@droqen.com>
2024-05-10 14:18:28 -0400

    final-ish map tweak

--- bomberman_project/dev/droqen/world_gen.gd
+++ bomberman_project/dev/droqen/world_gen.gd
@@ -2,14 +2,16 @@ extends Node
 
 const BLOCK = preload("res://gameplay/basic_block.tscn")
 onready var w = get_parent()
+onready var world_width : int = 13
+onready var world_height : int = 9
 
 func _ready():
-	for x in range(-10,10+1):
-		for y in range(-10,10+1):
+	for x in range(-world_width,world_width+1):
+		for y in range(-world_height,world_height+1):
 			var solid : int = 0
 			if posmod(x, 2) == 1: solid += 1
 			if posmod(y, 2) == 1: solid += 1
-			if abs(x)==10 or abs(y)==10: solid = 2
+			if abs(x)==world_width or abs(y)==world_height: solid = 2
 			if solid == 2 or (solid == 1 and randf() < 0.25):
 				spawn_block_at(x,y)
 
