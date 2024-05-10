6c7dca1c2619b289a839526aae26ce43e5f519d4
droqen <hi@droqen.com>
2024-05-10 14:12:08 -0400

    bunny has facing

--- bomberman_project/dev/droqen/GridMover.gd
+++ bomberman_project/dev/droqen/GridMover.gd
@@ -1,5 +1,7 @@
 extends GridObject
 
+var goalfacedir : Vector3 = Vector3.RIGHT
+
 func _physics_process(_delta):
 	var dpad = Vector2(
 		(1 if Input.is_action_just_pressed("ui_right") else 0) - (1 if Input.is_action_just_pressed("ui_left") else 0),
@@ -9,7 +11,15 @@ func _physics_process(_delta):
 		if self.try_move(dpad):
 			pass
 		else:
-			translate(
-				0.5 * GameGrid.cell_to_pos(dpad)
-			)
-		
+			translation += 0.5 * GameGrid.cell_to_pos(dpad)
+	look_at(translation + goalfacedir, Vector3.UP)
+
+func try_move(dir):
+	if dir:
+		goalfacedir = GameGrid.cell_to_pos(dir)
+		if .try_move(dir):
+			return true
+		else:
+			return false
+	else:
+		return false
