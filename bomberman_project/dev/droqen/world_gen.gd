d350371563af0be4041bbb59dbb6fbc39dfbdb52
droqen <hi@droqen.com>
2024-05-10 14:14:54 -0400

    a real maze

--- bomberman_project/dev/droqen/world_gen.gd
+++ bomberman_project/dev/droqen/world_gen.gd
@@ -6,7 +6,11 @@ onready var w = get_parent()
 func _ready():
 	for x in range(-10,10+1):
 		for y in range(-10,10+1):
-			if randf() < 0.25:
+			var solid : int = 0
+			if posmod(x, 2) == 1: solid += 1
+			if posmod(y, 2) == 1: solid += 1
+			if abs(x)==10 or abs(y)==10: solid = 2
+			if solid == 2 or (solid == 1 and randf() < 0.25):
 				spawn_block_at(x,y)
 
 func spawn_block_at(x,y):
