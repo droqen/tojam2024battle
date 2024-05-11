432e5ef0ee2534a5ced0699ca48f434731606e40
JesseLugaLuga <oongaboongagithub@gmail.com>
2024-05-11 12:01:51 -0400

    i will stop leaving notes unless they are notable

--- bomberman_project/dev/droqen/MultiplayerSpawner.gd
+++ bomberman_project/dev/droqen/MultiplayerSpawner.gd
@@ -3,7 +3,7 @@ extends MultiplayerSpawner
 @export var playerScene : PackedScene
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#spawn_function = spawnPlayer
+	spawn_function = spawnPlayer
 	if is_multiplayer_authority():
 		spawn(1)
 		multiplayer.peer_connected.connect(spawnPlayer)
